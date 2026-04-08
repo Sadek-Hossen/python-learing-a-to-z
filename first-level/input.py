6ea66d7751968916877e9737282fc2e58c681ae1
Sadek-Hossen <hossensadek726@gmail.com>
2026-04-08 13:11:43 +0600

    user input learned herer

---
+++ first-level/input.py
@@ -0,0 +1,11 @@
+# input from user  
+name = input('Writer your name :')
+roll = input('Here is your roll :')
+address = input('Here is your address :')
+gender = input('Here is your gender :')
+print(name)
+print(roll)
+
+    
+print(address)
+print(gender)
---
+++ first-level/variable.py
@@ -0,0 +1,17 @@
+
+name = 'Monna vai';
+age = 22;
+address = "cox's bazar";
+gender = "male";
+
+
+
+print('his name is :' + name);
+print('his age is ' ,age, ' year');
+print('his adress is :' + address);
+print('his gender is: '+gender);
+
+# add interger value by use comma in print function
+name = 'emon vai'; roll = 33; address = "cox's bazar"
+
+print('his name is :' + name+ 'and his roll is :'  ,roll, '\n' + "his address is : "+address);
