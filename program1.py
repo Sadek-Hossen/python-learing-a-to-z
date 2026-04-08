54e09cea1a0023ac57333b05dad5792c58343193
Sadek-Hossen <hossensadek726@gmail.com>
2026-04-08 12:51:46 +0600

    first commit for varable file

---
+++ program1.py
@@ -0,0 +1,12 @@
+
+name = 'Monna vai';
+age = 22;
+address = "cox's bazar";
+gender = "male";
+
+
+
+print('his name is :' + name);
+print('his age is ' ,age, ' year');
+print('his adress is :' + address);
+print('his gender is: '+gender);
